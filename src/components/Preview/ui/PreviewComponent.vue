456d21806950dc5a9b67fa2e3750257906a99fc5
a.skityaeva <a.skityaeva@itcodegroup.ru>
2024-09-22 01:15:41 +0500

    Удаление лишнего блока

--- src/components/Preview/ui/PreviewComponent.vue
+++ src/components/Preview/ui/PreviewComponent.vue
@@ -9,26 +9,24 @@ const { previewInfo } = storeToRefs(previewInfoStore);
 </script>
 
 <template>
-  <div class="page-content">
-    <div v-if="previewInfo">
-      <div class="item">
-        <h2 class="header">Персональные данные</h2>
-        <p class="info-text">
-          {{ defineFullName(previewInfo) }},
-          {{ defineYearsText(previewInfo.age) }}
-        </p>
-      </div>
-      <div v-if="previewInfo?.children?.length" class="item">
-        <h2 class="header">Дети</h2>
-        <div v-for="(child, index) in previewInfo.children" :key="index">
-          <ElTag class="tag">
-            <span class="info-text">
-              {{ child.name }}, {{ defineYearsText(child.age) }}
-            </span>
-          </ElTag>
-        </div>
+  <div v-if="previewInfo" class="page-content">
+    <div class="item">
+      <h2 class="header">Персональные данные</h2>
+      <p class="info-text">
+        {{ defineFullName(previewInfo) }},
+        {{ defineYearsText(previewInfo.age) }}
+      </p>
+    </div>
+    <div v-if="previewInfo?.children?.length" class="item">
+      <h2 class="header">Дети</h2>
+      <div v-for="(child, index) in previewInfo.children" :key="index">
+        <ElTag class="tag">
+          <span class="info-text">
+            {{ child.name }}, {{ defineYearsText(child.age) }}
+          </span>
+        </ElTag>
       </div>
     </div>
-    <p v-else>Нет данных</p>
   </div>
+  <p v-else>Нет данных</p>
 </template>
